59003b68d78aaf7d8fc64383ff8ed43966325555
yorhodes <yorkerhodesiv@gmail.com>
2018-09-04 13:22:58 -0500

    Add comments

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const trelloFetch = async (route, method="GET", data={}) => {
     const request = {
         url: "https://api.trello.com/1" + route,
         method: method,
+        // authenticate trello API
         params: {
             key: config.trello.api.key,
             token: config.trello.api.token
@@ -22,19 +23,28 @@ const trelloFetch = async (route, method="GET", data={}) => {
 }
 
 (async () => {
+    // fetch target board from user
     const boards = await trelloFetch("/member/me/boards")
     const board = boards.find(board => (board.name == config.trello.boardName))
-    const labels = await trelloFetch("/boards/" + board.id + "/labels")
-    var labelMap = {}
-    labels.forEach(label => labelMap[label.name] = label.id)
+
+    // fetch target list from board
     const lists = await trelloFetch("/boards/" + board.id + "/lists")
     const list = lists.find(list => (list.name == config.trello.listName))
 
+    // fetch labels form board
+    const labels = await trelloFetch("/boards/" + board.id + "/labels")
+
+    // map labels from name to id
+    var labelMap = {}
+    labels.forEach(label => labelMap[label.name] = label.id)
+
+    // parse canvas calendar
     icsParse.fromURL(config.canvasIcsUrl, {}, (err, events) => {
         if (err) console.log(err)
         const assignmentKeys = Object.keys(events).filter(
             key => key.startsWith("event-assignment")
         )
+        // iterate over assignments from calendar
         assignmentKeys.forEach(async (key) => {
             const event = events[key]
             const summary = event.summary
@@ -43,6 +53,7 @@ const trelloFetch = async (route, method="GET", data={}) => {
             const course = summary.substring(splitIdx + 1, splitIdx + 9)
 
             if (!course in labelMap) {
+                // create label for course
                 const resp = await trelloFetch("/labels", "POST", {
                     name: course,
                     color: "yellow",
@@ -51,6 +62,7 @@ const trelloFetch = async (route, method="GET", data={}) => {
                 labelMap[course] = resp.id
             }
 
+            // create card
             await trelloFetch("/cards", "POST", {
                 idList: list.id,
                 name: name,
