935319f0472ca68e382d7fce76c13a9cb54ba06a
yorhodes <yorkerhodesiv@gmail.com>
2018-09-26 15:29:14 -0500

    Manage diff more intelligently

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const diff = require("./assignmentDiff.json")
 const icsParse = require("node-ical")
 const fs = require("fs")
 
+const logAndExit = (errStr) => {
+    console.log(errStr)
+    process.exit(1)
+}
+
 const trelloFetch = async (route, method="GET", data={}) => {
     const request = {
         url: "https://api.trello.com/1" + route,
@@ -19,6 +24,7 @@ const trelloFetch = async (route, method="GET", data={}) => {
         response = await axios.request(request)
         return response.data
     } catch(err) {
+        logAndExit()
         console.log("Error Encountered:")
         console.log("Request: \n" + JSON.stringify(request, null, 2))
         console.log("Error: \n" + JSON.stringify(err, null, 2))
@@ -26,59 +32,60 @@ const trelloFetch = async (route, method="GET", data={}) => {
     }
 }
 
+const parseCanvasEventsForNew = (events) => {
+    // filter events on assignment type
+    const assignmentKeys = Object.keys(events).filter(key => 
+        key.startsWith("event-assignment")
+    )
+
+    // Define new assignments and update old assignments in memory
+    const newAssignments = assignmentKeys.filter(key => !diff.keys.includes(key))
+    diff.keys = assignmentKeys
+    fs.writeFile("assignmentDiff.json", JSON.stringify(diff, null, 2), (err) => {
+        if (err) throw err
+        console.log("Parsed assignment keys saved locally")
+    })
+
+    return newAssignments
+}
+
 (async () => {
     // fetch target board from user
     const boards = await trelloFetch("/member/me/boards")
     const board = boards.find(board => (board.name == config.trello.boardName))
 
-    if (typeof board === "undefined") {
-        console.log("Target board: " + config.trello.boardName + " was not found")
-        return
-    }
-
+    if (board === undefined)
+        logAndExit("Target board: " + config.trello.boardName + " was not found")
 
     // fetch target list from board
     const lists = await trelloFetch("/boards/" + board.id + "/lists")
     const list = lists.find(list => (list.name == config.trello.listName))
 
-    if (typeof list === "undefined") {
-        console.log("Target list: " + config.trello.listName + " was not found")
-        return
-    }
+    if (list === undefined)
+        logAndExit("Target list: " + config.trello.listName + " was not found")
 
-    // fetch labels form board
+    // fetch labels from board
     const labels = await trelloFetch("/boards/" + board.id + "/labels")
 
     // map labels from name to id
     var labelMap = {}
     labels.forEach(label => labelMap[label.name] = label.id)
 
-    // parse canvas calendar
+    // get all events from canvas calendar
     icsParse.fromURL(config.canvasIcsUrl, {}, (err, events) => {
-        if (err) console.log(err)
+        if (err)
+            logAndExit(err)
 
-        // filter events on assignment type and unparsed
-        const assignmentKeys = Object.keys(events).filter(key => 
-            key.startsWith("event-assignment") &&
-            !diff.keys.includes(key)
-        )
-
-        // update parsed
-        diff.keys = diff.keys.concat(assignmentKeys)
-        fs.writeFile("assignmentDiff.json", JSON.stringify(diff, null, 2))
-
-        // iterate over assignments from calendar
-        assignmentKeys.forEach(async (key) => {
+        // iterate over new assignments
+        parseCanvasEventsForNew(events).forEach(async (key) => {
             const event = events[key]
             const summary = event.summary
             const splitIdx = summary.indexOf("[")
             const name = summary.substring(0, splitIdx)
             const course = summary.substring(splitIdx + 1, splitIdx + 9)
 
-            if (!course in labelMap) {
-                console.log("No label exists called: " + course)
-                return
-            }
+            if (!course in labelMap)
+                logAndExit("No label exists called: " + course)
 
             // create card
             await trelloFetch("/cards", "POST", {
@@ -91,4 +98,3 @@ const trelloFetch = async (route, method="GET", data={}) => {
         })
     })
 })()
-
